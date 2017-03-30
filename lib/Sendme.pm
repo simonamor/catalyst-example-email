6f85176f73516291cdbe9f3fbf07326b2f60cc37
Simon Amor <simon@leaky.org>
2017-03-30 16:59:10 +0100

    Message sending

--- lib/Sendme.pm
+++ lib/Sendme.pm
@@ -40,6 +40,9 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
+
+    email_transport => 'Email::Sender::Transport::Test',
+
 );
 
 # Start the application
--- lib/Sendme/Controller/Root.pm
+++ lib/Sendme/Controller/Root.pm
@@ -2,6 +2,12 @@ package Sendme::Controller::Root;
 use Moose;
 use namespace::autoclean;
 
+use Email::MIME::Kit;
+use HTML::Entities;
+
+use Email::Sender::Simple;
+use Email::Sender::Transport::Test;
+
 BEGIN { extends 'Catalyst::Controller' }
 
 #
@@ -31,8 +37,37 @@ The root page (/)
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    my $html = <<EOF;
+<p>Enter your email address and click Send</p>
+
+<form method="POST" action="/">
+<input type="text" name="email" value="">
+<input type="submit" value="Send!">
+</form>
+EOF
+
+    if (my $email = $c->request->params->{ email }) {
+
+        my $name = $c->request->params->{ name } || "anonymous";
+
+        # Send a test email
+
+        my $kit = Email::MIME::Kit->new(
+            source => $c->path_to('email', 'msg1.mkit'),
+        );
+        my $msg = $kit->assemble({
+            yourname => $name,
+        });
+
+        my $transport = $c->config->{ email_transport }->new();
+        Email::Sender::Simple->send($msg, {
+            to => $email,
+            transport => $transport,
+        });
+
+        $html = "<pre>" . HTML::Entities::encode_entities($msg->as_string) . "</pre>";
+    }
+    $c->response->body($html);
 }
 
 =head2 default
